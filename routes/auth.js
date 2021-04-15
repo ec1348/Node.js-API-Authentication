19fa74fb68af626de99458ac5e9d0949bf12956e
Ming Jhang Chen <ec1348@gmail.com>
2021-04-15 18:38:28 +0800

    seperate validation to another js file

--- routes/auth.js
+++ routes/auth.js
@@ -1,30 +1,12 @@
 const router = require('express').Router();
 const User = require('../model/Users');
-
-
-
-//Validation
-const Joi = require('@hapi/joi');
-const { response } = require('express');
-
-const schema = Joi.object({
-    name: Joi.string()
-        .min(6)
-        .required(),
-    email: Joi.string()
-        .min(6)
-        .required()
-        .email(),
-    password: Joi.string()
-        .min(6)
-        .required()
-});
+const {registerValidation} = require('../validation');
 
 
 router.post('/register', async (req, res) =>{
 
     //Let's validate the data before save a user
-    const {error} = schema.validate(req.body);
+    const {error} = registerValidation(req.body);
     if(error) return res.status(400).send(error.details[0].message);
 
     const user = new User({
